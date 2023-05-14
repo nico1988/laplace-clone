71297f547a6aa932cac17669f68a8ed478e5c44b
nico1988 <410827024@qq.com>
2023-05-14 09:08:05 +0800

    feat: add bg

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,20 @@ export default function Index() {
   return (
     <main>
       <nav />
-      <div className="mt-20">
+      <div className="absolute top-20 z-10 w-full text-center">
         <h1 className="mb-3 cursor-pointer text-center text-4xl">LAPLACE</h1>
         <div className="cursor-pointer text-center">奶绿只是想偷偷睡一会</div>
       </div>
+      {/* give me bg-container with image full screen for background */}
+      <div className="absolute top-0 left-0 right-0 bottom-0">
+        <Image
+          className="h-full w-full object-cover"
+          src="/bg-original.jpeg"
+          alt="bg-original"
+          layout="fill"
+        />
+      </div>
+
       <div className="absolute bottom-0 flex w-full justify-center">
         <Image
           className="left-0 right-0 m-auto"
