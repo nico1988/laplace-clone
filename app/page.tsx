c69854293ac37123db5102c2fee0f4f509c885f2
nico1988 <410827024@qq.com>
2023-05-14 09:11:21 +0800

    feat: add text size and bg opacity

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ export default function Index() {
   return (
     <main>
       <nav />
-      <div className="absolute top-20 z-10 w-full text-center">
-        <h1 className="mb-3 cursor-pointer text-center text-4xl">LAPLACE</h1>
-        <div className="cursor-pointer text-center">奶绿只是想偷偷睡一会</div>
+      <div className="absolute top-20 z-10 w-full text-center text-[#b0baba]">
+        <h1 className="mb-3 cursor-pointer text-center text-[64px]">LAPLACE</h1>
+        <div className="cursor-pointer text-center text-[24px]">
+          奶绿只是想偷偷睡一会
+        </div>
       </div>
       {/* give me bg-container with image full screen for background */}
-      <div className="absolute top-0 left-0 right-0 bottom-0">
+      <div className="absolute top-0 left-0 right-0 bottom-0 opacity-50">
         <Image
           className="h-full w-full object-cover"
           src="/bg-original.jpeg"
