3a3487b9bc8b8bc7872716d9ac8ee70aef46d930
StayAndGo <wthuck@163.com>
2023-05-15 13:54:24 +0800

    feat: init calendar page

---
+++ app/calendar/page.tsx
@@ -0,0 +1,68 @@
+export default function CalendarPage() {
+  const mockSummaryInfo = {
+    total: 156,
+    history: '新账号开通直播间 —2022-05-15 直播',
+  };
+
+  const mockCategoryConfig = {
+    talk: {
+      title: '杂谈',
+      color: '#FFC55',
+    },
+    game: {
+      title: '游戏',
+      color: '#47ebeb',
+    },
+    watching: {
+      title: '观影',
+      color: '#c67de8',
+    },
+    submission: {
+      title: '投稿',
+      color: '#EA8ABA',
+    },
+    music: {
+      title: '歌回',
+      color: '#3EE081',
+    },
+    exercise: {
+      title: '健身',
+      color: '#F05C77',
+    },
+    radio: {
+      title: '电台',
+      color: '#AFED5A',
+    },
+    collaborative: {
+      title: '联动',
+      color: '#FD9A49',
+    },
+    special: {
+      title: '特殊',
+      color: '#FFDD33',
+    },
+  };
+
+  /* TODO
+   *  mock每天的数据需要包含：日期，当日内容des，link（如果有），type： ’watching‘等分类
+   *  以周为list
+   */
+
+  // TODO 字体使用全局样式 每个div最小宽度80px
+  return (
+    <div className="flex h-screen flex-col items-center px-4">
+      {/* page title */}
+      <div className="flex flex-col items-center">
+        <div className="text-[64px]">Schedule</div>
+        <div className=" text-2xl">活动/直播日程</div>
+      </div>
+      <div className=" w-full">
+        <div>
+          {/* TODO 链接 */}
+          共直播{mockSummaryInfo.total}场/iCal <a href="calendar">日历订阅</a>
+        </div>
+        <div>历史上的今天：{mockSummaryInfo.history}</div>
+      </div>
+    </div>
+  );
+}
