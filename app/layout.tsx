9a6af4cb601fb0d2a639b6bc2e89e33dd8fc22bd
StayAndGo <wthuck@163.com>
2023-05-19 00:02:57 +0800

    fix: 修改全局样式

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css';
+import Image from 'next/image';
+import NavBar from '@/components/NavBar';
 
 export default function RootLayout({
   children,
@@ -8,7 +10,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body>
+        <NavBar />
+        <div className="absolute top-0 left-0 right-0 bottom-0 opacity-50">
+          <Image
+            className="h-full w-full object-cover"
+            src="/bg-original.jpeg"
+            alt="bg-original"
+            layout="fill"
+          />
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,14 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 
-// components
-import NavBar from '@/components/NavBar';
-
 export default function Index() {
   return (
     <main>
-      <NavBar />
       <div className="absolute top-20 z-10 w-full text-center text-[#b0baba]">
         <h1 className="mb-3 cursor-pointer text-center text-[64px]">LAPLACE</h1>
         <div className="cursor-pointer text-center text-[24px]">
           奶绿只是想偷偷睡一会
         </div>
-      </div>
-
-      <div className="absolute top-0 left-0 right-0 bottom-0 opacity-50">
-        <Image
-          className="h-full w-full object-cover"
-          src="/bg-original.jpeg"
-          alt="bg-original"
-          layout="fill"
-        />
       </div>
 
       <div className="absolute bottom-0 flex w-full justify-center">
