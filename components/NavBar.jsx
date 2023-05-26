ff897c9f9661cdb67237e6589d81fbfd6e9795a0
StayAndGo <wthuck@163.com>
2023-05-27 03:30:00 +0800

    feat: 添加更多下拉菜单

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 
 export default function NavBar() {
@@ -23,7 +25,18 @@ export default function NavBar() {
       title: '关于',
       href: 'about',
     },
+    {
+      icon: '/more.svg',
+      title: '更多',
+      href: '',
+    },
   ];
+  const handleDropdownClick = () => {
+    const menu = document.querySelector('.dropdown');
+    const visible = menu.style.display || 'none';
+
+    menu.style.display = visible !== 'none' ? 'none' : 'block';
+  };
 
   return (
     <nav className="nav__wrapper z-50 mt-3">
@@ -41,17 +54,45 @@ export default function NavBar() {
             src="/weibo.svg"
             width={fontSize}
             height={fontSize}
-            b
+            bb
             alt="weibo"
           />
         </a>
         <span>|</span>
         {NavList.map((item) => (
           <li key={item.title} className="flex items-center">
-            <a href={item.href} className="flex">
-              <Image src={item.icon} width={fontSize} height={fontSize} />
-              {item.title}
-            </a>
+            {item.href ? (
+              <a href={item.href} className="flex">
+                <Image src={item.icon} width={fontSize} height={fontSize} />
+                {item.title}
+              </a>
+            ) : (
+              <div className="relative">
+                <button
+                  key={item.title}
+                  className="flex cursor-pointer"
+                  onClick={handleDropdownClick}
+                >
+                  <Image src={item.icon} width={fontSize} height={fontSize} />
+                  <span>{item.title}</span>
+                </button>
+                <div className="dropdown absolute flex hidden w-36 rounded-md bg-[#20231d] px-1 py-2">
+                  <a
+                    className="flex w-full gap-x-1 rounded px-1 py-1 hover:bg-[#3e4139]"
+                    href="https://clock.laplace.live/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Image
+                      src="/clock.svg"
+                      width={fontSize}
+                      height={fontSize}
+                    />
+                    OBS时钟
+                  </a>
+                </div>
+              </div>
+            )}
           </li>
         ))}
       </ul>
